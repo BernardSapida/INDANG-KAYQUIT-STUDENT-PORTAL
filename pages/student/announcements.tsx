8284b03fa1a0cdbbe6692dab9ea30434113efbb6
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-08-19 11:08:07 +0800

    Update: Announcements page for student

--- pages/student/announcements.tsx
+++ pages/student/announcements.tsx
@@ -1,23 +1,26 @@
-// Next Modules
-import Link from "next/link";
-
 // React-Icons
 import { FaGraduationCap } from 'react-icons/fa';
 
 // Components
+import Event from "@/components/student/announcements/Event";
 
+// Utilities
+import { getAcademicYear } from "@/utils/date/date";
 
 // CSS
-import style from "@/public/css/signin.module.css";
+import style from "@/public/css/student-announcements.module.css";
 
 function Announcements() {
     return (
         <div className="mb-5">
             <div className={`${style.title}`}>
-                <h1><FaGraduationCap /> Class Announcement(s) 2023-2024</h1>
+                <h1><FaGraduationCap /> Class Announcement(s) {getAcademicYear()}</h1>
             </div>
             <div className={`${style.container}`}>
                 {/* Components / Contents Goes here */}
+                <Event />
+                <Event />
+                <Event />
             </div>
         </div>
     );
