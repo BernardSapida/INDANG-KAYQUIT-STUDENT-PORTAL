515e966acf84cee02d260d19029d092c48543965
Bernard Sapida <BERNARDSAPIDA1706@GMAIL.COM>
2023-08-24 16:19:02 +0800

    Update: News component renamed

--- pages/student/dashboard.tsx
+++ pages/student/dashboard.tsx
@@ -6,7 +6,7 @@ import { getSession } from "next-auth/react";
 
 // Components
 import Cards from "@/components/student/dashboard/Cards";
-import News from "@/components/student/dashboard/news";
+import News from "@/components/student/dashboard/News";
 
 // CSS
 import style from "@/public/css/student-dashboard.module.css";
